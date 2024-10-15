aa70dd996cabb861f41afb9394567ee49e50fa39
Pinaki <pinakiprasadrajkumar@gmail.com>
2024-10-15 12:33:59 +0530

    same font size and mobile optimization

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -174,11 +174,11 @@ const LoginPage = ({ handleLogin }) => {
         }}
       /> */}
       <main className="login-panel">
-        <div className="bg-[#004C8A] flex flex-col items-center md:w-[578px]" style={{ borderRadius: '24px 0px 0px 24px' }}>
+        <div className="main-container-div bg-[#004C8A] flex flex-col items-center md:w-[578px]">
           <div className="p-8">
             <p style={{fontSize: '35px',fontWeight: '700',color: '#f8f8ff'}}>Excel Connect<br/><span style={{fontSize: '30px',fontWeight: '0',color: '#f8f8ff'}}>Reconnect with your Alma Mater</span></p>
             {/* <p className="your-alumni-journey">Reconnect with your Alma Mater</p> */}
-            <div style={{ fontSize: '0.26em', color: 'white',paddingTop: '25px' }}>
+            <div className='message-div' style={{ fontSize: '0.26em', color: 'white',paddingTop: '25px' }}>
               <p>Dear Excellites,</p><br />
               <p>Welcome back to your EPS!</p><br />
               <p>As a member of our vibrant and accomplished alumni community, you are an integral part of the legacy we continue to build. Whether you walked through our portals years ago or just recently graduated, your time at Excel Public School shaped your future and contributed to the school's growth and success.</p><br />
